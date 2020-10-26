e6dfb6b65fe21e605654a2f3cea656ed293b193e
rochimatus <rochimatus22@gmail.com>
2020-10-27 00:19:34 +0700

    Fix layouting register page + api set Fixes #4

--- react-basic/src/constants/urls.js
+++ react-basic/src/constants/urls.js
@@ -5,6 +5,7 @@
 const BASE_URL = "http://localhost:8000/api";
 
 export const LOGIN_API =  `${BASE_URL}/auth/login`;
+export const REGISTER_API =  `${BASE_URL}/auth/register`;
 
 export const GET_FILMS = "https://ghibliapi.herokuapp.com/films";
 
--- react-basic/src/pages/Register.js
+++ react-basic/src/pages/Register.js
@@ -5,93 +5,272 @@
 */
 
 import React from "react";
-import { Button, Container, Navbar, Form, Alert, Image } from "react-bootstrap";
+import { Button, Row, Form, InputGroup, Image } from "react-bootstrap";
 import { Link, Redirect } from "react-router-dom";
-import { login } from "utils/auth";
+import { setUserLogin } from "utils/auth";
 import headerImage from '../images/pasien/REGISTER.png';
 import logo from '../images/pasien/logo.png';
+import axios from "axios";
+import { REGISTER_API } from "constants/urls";
+import Loader from 'react-loader-spinner';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { faUser, faPhone, faKey, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+library.add(fab, faUser, faPhone, faKey, faEnvelope)
 
-const Login = () => {
+const Register = () => {
+  const [role, setRole] = React.useState("");
+  const [name, setName] = React.useState("");
+  const [phone, setPhone] = React.useState("");
   const [email, setEmail] = React.useState("");
   const [password, setPassword] = React.useState("");
-  const [error, setError] = React.useState(false);
+  const [passwordConfirmation, setPasswordConfirmation] = React.useState("");
+  const [errorName, setErrorName] = React.useState("");
+  const [errorPhone, setErrorPhone] = React.useState("");
+  const [errorEmail, setErrorEmail] = React.useState("");
+  const [errorPassword, setErrorPassword] = React.useState("");
+  const [errorPasswordConfirmation, setErrorPasswordConfirmation] = React.useState("");
   const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
 
   React.useEffect(() => {
-    if (email || password) {
-      setError(false);
+    if (name) {
+      setErrorName("");
+    }
+    if (phone) {
+      setErrorPhone("");
+    }
+    if (email) {
+      setErrorEmail("");
+    }
+    if(password){
+      setErrorPassword("");
+    }
+    if(passwordConfirmation){
+      setErrorPasswordConfirmation("");
     }
     return () => {};
-  }, [email, password]);
+  }, [name, phone, email, password, passwordConfirmation]);
 
   const _onSubmit = () => {
-    if (email === "angga.ganteng@email.com" && password === "123") {
-      login({
+    setLoading(true)
+    axios
+      .post(REGISTER_API, {
+        name: name,
+        phone: phone,
         email: email,
+        password: password,
+        password_confirmation: passwordConfirmation,
+        role: "Pasien",
+      })
+      .then((res) => {
+        setUserLogin({
+          token: res.data.access_token,
+          email: res.data.user.email,
+          role: res.data.user.role
+        });
+        setIsLoggedIn(true)
+        setLoading(false)
+      })
+      .catch((err) => {
+        console.log(err)
+        if(err.response){
+          if(err.response.data?.error){
+            setErrorEmail(err.response.data?.error ? err.response.data.error : "");
+          }else if(err.response.data?.email || err.response.data?.password){
+            setErrorEmail(err.response.data?.email ? err.response.data.email : "");
+            setErrorPassword(err.response.data?.password ? err.response.data.password : "");
+          }
+        } else{
+          setErrorEmail(JSON.stringify(err))
+        }
+        setLoading(false)
       });
-      setIsLoggedIn(true);
-    } else {
-      setError(true);
-    }
   };
 
   return (
-    <div className="bg-app" style={{backgroundColor: "#E8F9F3"}}>
+    <div className="bg-app pr-3">
       {isLoggedIn && <Redirect to="/dashboard" />}
-      {/* <Navbar bg="light" expand="lg">
-        <Container>
-          <Link to="/">
-            <Navbar.Brand color="white">Login</Navbar.Brand>
-          </Link>
-        </Container>
-      </Navbar> */}
-      <div className="row">
-        <Container className="col-lg-9">
+      <Row xs="auto">
+        <div className="col-lg-9 pr-0">
           <Image src={headerImage} fluid/>
-        </Container>
-        <Container className="col-lg-3">
-          <div className="mt-3 mb-3">
-            <Image src={logo}/>
+        </div>
+        <div className="col-lg-3 pl-4" fluid >
+          <div className="mt-5 mb-5 text-center">
+            <Image src={logo} width="50%"/>
           </div>
           <Form>
-            <Alert variant="primary">
-              <span style={{ fontWeight: "bold" }}>Email: </span>
-              angga.ganteng@email.com,
-              <span style={{ fontWeight: "bold" }}> Password: </span>
-              123
-            </Alert>
-            {error && <Alert variant="danger">Salah bos</Alert>}
+          <Form.Group controlId="formBasicName">
+            <InputGroup className="align-items-center"
+                style={{
+                  backgroundColor:"#fff",
+                  borderRadius:10,
+                  }}>
+                  <span className="px-2">
+                    <FontAwesomeIcon icon="user" />
+                  </span>
+                  <Form.Control style={{
+                    borderColor:"#fff",
+                    borderTopRightRadius:10,
+                    borderBottomRightRadius:10,
+                    paddingLeft:4
+                  }}
+                    type="text"
+                    placeholder="Name"
+                    value={name}
+                    onChange={(e) => {
+                      setName(e.target.value)
+                      setErrorName("")
+                    }}
+                  />
+              </InputGroup>
+              
+            {errorName !== "" ? <span className="text-danger ml-2">{errorName}</span> : ""}
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPhone">
+            <InputGroup className="align-items-center"
+                style={{
+                  backgroundColor:"#fff",
+                  borderRadius:10,
+                  }}>
+                  <span className="px-2">
+                    <FontAwesomeIcon icon="phone" />
+                  </span>
+                  <Form.Control style={{
+                    borderColor:"#fff",
+                    borderTopRightRadius:10,
+                    borderBottomRightRadius:10,
+                    paddingLeft:4
+                  }}
+                    type="text"
+                    placeholder="Phone Number"
+                    value={phone}
+                    onChange={(e) => {
+                      setPhone(e.target.value)
+                      setErrorPhone("")
+                    }}
+                  />
+              </InputGroup>
+              
+            {errorPhone !== "" ? <span className="text-danger ml-2">{errorPhone}</span> : ""}
+            </Form.Group>
 
             <Form.Group controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="Enter email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-              </Form.Text>
+            <InputGroup className="align-items-center"
+                style={{
+                  backgroundColor:"#fff",
+                  borderRadius:10,
+                  }}>
+                  <span className="px-2">
+                    <FontAwesomeIcon icon="envelope" />
+                  </span>
+                  <Form.Control style={{
+                    borderColor:"#fff",
+                    borderTopRightRadius:10,
+                    borderBottomRightRadius:10,
+                    paddingLeft:4
+                  }}
+                    type="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => {
+                      setEmail(e.target.value)
+                      setErrorEmail("")
+                    }}
+                  />
+              </InputGroup>
+              
+            {errorEmail !== "" ? <span className="text-danger ml-2">{errorEmail}</span> : ""}
             </Form.Group>
 
             <Form.Group controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                type="password"
-                placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
+              <InputGroup className="align-items-center"
+                style={{
+                  backgroundColor:"#fff",
+                  borderRadius:10,
+                  }}>
+                  <span className="px-2">
+                    <FontAwesomeIcon icon="key" />
+                  </span>
+                  <Form.Control style={{
+                    borderColor:"#fff",
+                    borderTopRightRadius:10,
+                    borderBottomRightRadius:10,
+                    paddingLeft:4
+                  }}
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => {
+                      setPassword(e.target.value)
+                      setErrorPassword("")
+                    }}
+                  />
+              </InputGroup>
+              
+              {errorPassword !== "" ? <span className="text-danger ml-2">{errorPassword}</span> : ""}
             </Form.Group>
-            <Button variant="primary" onClick={_onSubmit}>
-              Submit
-            </Button>
+
+            <Form.Group controlId="formBasicPasswordConfirmation">
+              <InputGroup className="align-items-center"
+                style={{
+                  backgroundColor:"#fff",
+                  borderRadius:10,
+                  }}>
+                  <span className="px-2">
+                    <FontAwesomeIcon icon="key" />
+                  </span>
+                  <Form.Control style={{
+                    borderColor:"#fff",
+                    borderTopRightRadius:10,
+                    borderBottomRightRadius:10,
+                    paddingLeft:4
+                  }}
+                    type="password"
+                    placeholder="Password Confirmation"
+                    value={passwordConfirmation}
+                    onChange={(e) => {
+                      setPasswordConfirmation(e.target.value)
+                      setErrorPasswordConfirmation("")
+                    }}
+                  />
+              </InputGroup>
+              
+              {errorPasswordConfirmation !== "" ? <span className="text-danger ml-2">{errorPasswordConfirmation}</span> : ""}
+            </Form.Group>
+
+            <div className="row justify-content-center mb-5 mt-4">
+              <div className="col-md-12">
+                <Button disabled={loading} className="rounded-pill" variant="primary" block
+                onClick={_onSubmit}>
+                  {loading ? 
+                  <Loader
+                  type="Oval"
+                  color="#FFF"
+                  height={20}
+                  width={20}
+                  />
+                  :
+                  <span>Register</span>
+                  }
+                </Button>
+              </div>
+            </div>
           </Form>
-        </Container>
-      </div>
+          <div className="row justify-content-center">
+            <div className="col-md-12 text-secondary text-center">
+              Sudah punya akun? 
+              <span className="font-weight-bold">
+                <Link to="/register"> Sign In</Link>
+              </span>
+            </div>
+          </div>
+        </div>
+      </Row>
     </div>
   );
 };
 
-export default Login;
+export default Register;
