866cebea50046eb34ee7d089729ed59f9cc605af
Yanu Adi Nugraha <2110181004@msoffice.pens.ac.id>
2020-10-27 17:04:28 +0700

    User - List Poli Page Fixes #13

--- react-basic/src/constants/urls.js
+++ react-basic/src/constants/urls.js
@@ -8,3 +8,4 @@ export const LOGIN_API =  `${BASE_URL}/auth/login`;
 
 export const GET_FILMS = "https://ghibliapi.herokuapp.com/films";
 
+export const GET_POLYCLINIC_OF_HA = `${BASE_URL}/admin/health-agency/6/polyclinic`;
---
+++ react-basic/src/pages/ListPolyclinic.js
@@ -0,0 +1,67 @@
+import React from "react";
+import NavBar from "../components/NavBar";
+import { GET_POLYCLINIC_OF_HA } from "constants/urls";
+import axios from "axios";
+import { Row, Col, Card, Container, Breadcrumb } from "react-bootstrap";
+import { FaReact } from "react-icons/fa";
+
+const ListPolyclinic = () => {
+  const [polyclinics, setPolyclinics] = React.useState([]);
+  React.useEffect(() => {
+    axios
+      .get(GET_POLYCLINIC_OF_HA)
+      .then((res) => {
+        setPolyclinics(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <div>
+      {console.log(polyclinics)}
+      <NavBar></NavBar>
+      <Breadcrumb>
+        <Breadcrumb.Item href="#">Home</Breadcrumb.Item>
+        <Breadcrumb.Item href="https://getbootstrap.com/docs/4.0/components/breadcrumb/">
+          Library
+        </Breadcrumb.Item>
+        <Breadcrumb.Item active>Data</Breadcrumb.Item>
+      </Breadcrumb>
+      <div className="mx-5">
+        <h2>Daftar Poli</h2>
+      </div>
+      <Container className="mt-4">
+        <Card>
+          <Card.Body>
+            <Row>
+              {polyclinics.map((polyclinic,key) => {
+                return (
+                  <Card 
+                    key={key} 
+                    className="mx-3 text-center mx-auto"
+                    style={{
+                      backgroundColor : "#F0F5FE"
+                    }}
+                  >
+                    <Card.Body>
+                      <FaReact
+                        style={{
+                          fontSize : "30px"
+                        }}
+                      />
+                      <p className="mt-3">{polyclinic.poly_master.name}</p>
+                    </Card.Body>
+                  </Card>
+                );
+              })}
+            </Row>
+          </Card.Body>
+        </Card>
+      </Container>
+    </div>
+  );
+};
+
+export default ListPolyclinic;
