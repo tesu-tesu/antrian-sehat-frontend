364eea3a48a9d6ba7ed1619428da5320b2555133
ghozyghifari <ghozy.ghifari21@gmail.com>
2020-10-28 19:57:44 +0700

    error login url JWT Fixes #52

--- react-basic/src/constants/urls.js
+++ react-basic/src/constants/urls.js
@@ -7,7 +7,11 @@ const BASE_URL = "http://localhost:8000/api";
 
 export const GET_FILMS = "https://ghibliapi.herokuapp.com/films";
 
-export const JWT_HEADER = Cookies.getJSON("USER").token;
+let JWT = null;
+if(Cookies.getJSON("USER") != undefined)
+  JWT = Cookies.getJSON("USER").token;
+
+export const JWT_HEADER = JWT;
 export const LOGIN_API =  `${BASE_URL}/auth/login`;
 export const REGISTER_API =  `${BASE_URL}/auth/register`;
 
