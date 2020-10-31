fee8ab42921754aec06c1d0a8edd0a231b66fa26
Re <refanopradana@gmail.com>
2020-11-01 05:38:09 +0700

    antrian-sehat-frontend-79 Issue #79 Create Footer Component

--- react-basic/src/components/pasien/PasienContainer.js
+++ react-basic/src/components/pasien/PasienContainer.js
@@ -3,45 +3,47 @@
 */
 
 import React from "react";
-import { Card, Container, Row, Col, Breadcrumb } from "react-bootstrap";
+import {Breadcrumb, Card} from "react-bootstrap";
 import NavBar from "components/NavBar";
+import FooterBar from "../FooterBar";
 
 const PasienContainer = (props) => {
-  return (
-    <>
-        <div className="header">
-        <NavBar></NavBar>
-        </div>
-        <div className="pasien-body">
-            <Breadcrumb className="mx-3 mt-2" style={{
-                borderRadius: "20px",
-                backgroundColor: "#fff"
-            }}>
-                <Breadcrumb.Item href="/">
+    return (
+        <>
+            <div className="header">
+                <NavBar/>
+            </div>
+            <div className="pasien-body">
+                <Breadcrumb className="mx-3 mt-2" style={{
+                    borderRadius: "20px",
+                    backgroundColor: "#fff"
+                }}>
+                    <Breadcrumb.Item href="/">
                     <span>
-                        <i className="ni ni-shop mr-2" />
+                        <i className="ni ni-shop mr-2"/>
                         Home
                     </span>
-                </Breadcrumb.Item>
-                <Breadcrumb.Item active>
-                    {props.breadcrumb}
-                </Breadcrumb.Item>
-            </Breadcrumb>
-            <div className="body-title mx-4">
-                {props.title}
-            </div>
-            <div className="mx-4 mt-3">
-                <Card className="mx-lg-4" style={{
+                    </Breadcrumb.Item>
+                    <Breadcrumb.Item active>
+                        {props.breadcrumb}
+                    </Breadcrumb.Item>
+                </Breadcrumb>
+                <div className="body-title mx-4">
+                    {props.title}
+                </div>
+                <div className="mx-4 mt-3">
+                    <Card className="mx-lg-4" style={{
                         borderRadius: "15px"
                     }}>
                         <Card.Body>
                             {props.content}
                         </Card.Body>
-                </Card>
+                    </Card>
+                </div>
             </div>
-        </div>
-    </>
-  );
+            <FooterBar/>
+        </>
+    );
 };
 
 export default PasienContainer;
