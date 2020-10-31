3a94fc5b7d0e42d0687d04eed7e605bac69ecd9d
Re <refanopradana@gmail.com>
2020-10-31 15:16:05 +0700

    antrian-sehat-frontend-12 User - List Health Agencies page useEffect async await

--- react-basic/src/pages/ListHealthAgency.js
+++ react-basic/src/pages/ListHealthAgency.js
@@ -1,72 +1,72 @@
 import React, {useEffect, useState} from 'react';
-import NavBar from "../components/NavBar";
-import {Breadcrumb, Card, Container, Row} from "react-bootstrap";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {Card, Row} from "react-bootstrap";
 import Axios from "axios";
 import {GET_HEALTH_AGENCY, JWT_HEADER} from "../constants/urls";
-import { useParams } from "react-router";
+import {useParams} from "react-router";
+import {FaHospital} from "react-icons/fa";
+import MasterLayout from "../components/pasien/PasienContainer";
 
 const ListHealthAgency = () => {
     const [healthAgencies, setHealthAgencies] = useState([]);
     let {id_health_agency} = useParams();
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
-        console.log(JWT_HEADER)
-        Axios
-            .get(GET_HEALTH_AGENCY(id_health_agency), {
-                headers: {"Authorization": `Bearer ${JWT_HEADER}`}
-            }).then(r => {
-            setHealthAgencies(r.data);
-        }).catch(e => console.log(e))
+        const fetchData = async () => {
+            console.log(JWT_HEADER)
+            setIsLoading(true);
+            await Axios
+                .get(GET_HEALTH_AGENCY(id_health_agency), {
+                    headers: {"Authorization": `Bearer ${JWT_HEADER}`}
+                }).then(r => {
+                    setHealthAgencies(r.data);
+                }).catch(e => console.log(e))
+            setIsLoading(false);
+        };
+        fetchData()
+        //setIsLoading(false);
     }, []);
 
 
-    return (
-        <div>
-            <NavBar/>
-            <section>
-                <Breadcrumb>
-                    <Breadcrumb.Item href="/"> <FontAwesomeIcon icon={["far", "home"]}/>Home</Breadcrumb.Item>
-                    <Breadcrumb.Item href="#">
-                        puskesmas
-                    </Breadcrumb.Item>
-                </Breadcrumb>
-                <div className="mx-5">
-                    <h2 className="ui dividing header">Daftar Puskesmas</h2>
-                </div>
-            </section>
-            <Container className="mt-4">
-                <Card>
-                    <Card.Body>
-                        <Row>
-                            {
-                                healthAgencies.map((
-                                    healthAgency, key) => {
-                                        return (
-                                            <Card
-                                                key={key}
-                                                className="mx-3 text-center mx-auto"
+    const props = {
+        title: 'Daftar Puskesmas',
+        breadcrumb: 'daftar puskesmas',
+        content:
+            <Row>
+                {
+                    isLoading ? (
+                        <div>..is loading</div>
+                    ) : (
+                        healthAgencies.map((
+                            healthAgency, key) => {
+                                return (
+                                    <Card
+                                        key={key}
+                                        className="mx-3 text-center mx-auto"
+                                        style={{
+                                            backgroundColor: "#F0F5FE"
+                                        }}
+                                    >
+                                        {/*                                    <Card.Header>
+                                        <img src={healthAgency.image} className="card-img" alt="image"/>
+                                    </Card.Header>*/}
+                                        <Card.Body>
+                                            <FaHospital
                                                 style={{
-                                                    backgroundColor: "#F0F5FE"
+                                                    fontSize: "30px"
                                                 }}
-                                            >
-                                                <Card.Header>
-                                                    <img src={healthAgency.image} className="card-img" alt="image"/>
-                                                </Card.Header>
-                                                <Card.Body>
-                                                    <p className="mt-3">{healthAgency.name}</p>
-                                                </Card.Body>
-                                            </Card>
-                                        );
-                                    }
-                                )
+                                            />
+                                            <p className="mt-3">{healthAgency.name}</p>
+                                        </Card.Body>
+                                    </Card>
+                                );
                             }
-                        </Row>
-                    </Card.Body>
-                </Card>
-            </Container>
-        </div>
-    );
+                        )
+                    )
+                }
+            </Row>
+    };
+    return <MasterLayout {...props} />;
 };
 
 export default ListHealthAgency;
