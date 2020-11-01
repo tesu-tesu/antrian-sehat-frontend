0788a2e35db9bfd8759a99343b4c5c9ae1f9f2e7
satriyoaji <satriyoaji27@gmail.com>
2020-11-01 11:07:41 +0700

    User page - dashboard Fixes #77

--- react-basic/src/routes/patient_routes.js
+++ react-basic/src/routes/patient_routes.js
@@ -1,12 +1,14 @@
 import ListPolyclinic  from "pages/ListPolyclinic";
+import ListHealthAgency  from "pages/ListHealthAgency";
 import PasienContainer from "components/pasien/PasienContainer";
+import Home from "pages/Home";
 
 export const APP_PATIENT_ROUTE = [
   {
     name: "Home",
     path: "/",
     exact: true,
-    component: PasienContainer,
+    component: Home,
     private: true,
     isPasien: true,
   },
@@ -15,6 +17,14 @@ export const APP_PATIENT_ROUTE = [
     path: "/list-polyclinic/:id_health_agency",
     exact: true,
     component: ListPolyclinic,
+    private: true,
+    isPasien: true,
+  },
+  {
+    name: "Health Agency",
+    path: "/health-agency",
+    exact: true,
+    component: ListHealthAgency,
     private: true,
     isPasien: true,
   },
