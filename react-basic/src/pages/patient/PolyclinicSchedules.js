572edc220e5ca4e99386e03c0b7d6d7decf08edd
satriyoaji <satriyoaji27@gmail.com>
2020-11-09 12:19:33 +0700

    success print right days & date on thead Fixes #121

--- react-basic/src/pages/patient/PolyclinicSchedules.js
+++ react-basic/src/pages/patient/PolyclinicSchedules.js
@@ -5,13 +5,24 @@ import {
   GET_ONE_HEALTH_AGENCY,
 } from "constants/urls";
 import axios from "axios";
-import { Row, Col, Card, Table } from "react-bootstrap";
+import { Row, Col, Card, Table, Button } from "react-bootstrap";
 import { FaReact } from "react-icons/fa";
 import { useParams } from "react-router";
 
 const PolyclinicSchedules = () => {
   const [polyclinics, setPolyclinics] = React.useState([]);
   const [healthAgency, setHealthAgency] = React.useState([]);
+  const [schedule, setSchedule] = React.useState();
+  const [days, setDays] = React.useState([
+    "Minggu",
+    "Senin",
+    "Selasa",
+    "Rabu",
+    "Kamis",
+    "Jumat",
+    "Sabtu",
+  ]);
+  const [currentDate, setCurrentDate] = React.useState(new Date());
 
   let { id_health_agency } = useParams();
 
@@ -32,12 +43,41 @@ const PolyclinicSchedules = () => {
         headers: { Authorization: `Bearer ${JWT_HEADER}` },
       })
       .then((res) => {
+        console.log(res.data);
         setPolyclinics(res.data);
       })
       .catch((err) => {
         console.log(err);
       });
+
+    setSchedule();
   }, []);
+
+  const printDays = () => {
+    return days.map((day, key) => {
+      let dayCode = key - currentDate.getDay();
+      if (dayCode < 0) {
+        dayCode += 7;
+      }
+      let dateString = new Date(); //get next day
+      dateString.setDate(dateString.getDate() + dayCode);
+
+      let formattedDate = dateString
+        .toLocaleDateString("en-GB", {
+          day: "numeric",
+          month: "short",
+          year: "numeric",
+        })
+        .replace(/ /g, " ");
+      console.log(formattedDate);
+      return (
+        <th className="text-center">
+          {day}
+          <div>{formattedDate}</div>
+        </th>
+      );
+    });
+  };
 
   return (
     <div className="mx-4 mt-3">
@@ -62,28 +102,45 @@ const PolyclinicSchedules = () => {
                 <h4>
                   {healthAgency.name}: Pilih Jadwal dan Lakukan Pendaftaran
                 </h4>
-                <Table striped hover responsive="md">
+                <Table striped hover responsive>
                   <thead>
                     <tr>
-                      <th>Nama Poli</th>
-                      <th>Senin</th>
-                      <th>Selasa</th>
-                      <th>Rabu</th>
-                      <th>Kamis</th>
-                      <th>Jumat</th>
-                      <th>Sabtu</th>
-                      <th>Minggu</th>
+                      <th>Nama Poliklinik</th>
+                      {currentDate && printDays()}
                     </tr>
                   </thead>
                   <tbody>
                     {polyclinics.map((polyclinic, key) => {
-                      return (
-                        <tr>
-                          <td className="font-weight-600">
-                            {polyclinic.poly_master.name}
-                          </td>
-                        </tr>
-                      );
+                      if (polyclinic) {
+                        return (
+                          <tr>
+                            <td className="font-weight-600">
+                              {polyclinic.poly_master.name}
+                            </td>
+                            {days.map((day, index) => {
+                              polyclinic.sorted.map((schedule, idx) => {
+                                if (schedule.day == index) {
+                                  // console.log(index);
+                                  return (
+                                    <td className="text-center">
+                                      <div>
+                                        <Button size="sm" variant="primary">
+                                          Daftar
+                                        </Button>
+                                      </div>
+                                      <div>{schedule.time_open}</div>
+                                    </td>
+                                  );
+                                } else {
+                                  return <td>-</td>;
+                                }
+                              });
+                            })}
+                          </tr>
+                        );
+                      } else {
+                        return <td></td>;
+                      }
                     })}
                   </tbody>
                 </Table>
