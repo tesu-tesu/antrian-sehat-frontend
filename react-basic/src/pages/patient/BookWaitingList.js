2522d3426a1426d66cf339a08aab872b65e0a787
rochimatus <rochimatus22@gmail.com>
2020-11-12 10:08:08 +0700

    page to handle invalid date & schedule when user want to regist themselves in a poly Fixes #144

--- react-basic/src/pages/patient/BookWaitingList.js
+++ react-basic/src/pages/patient/BookWaitingList.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { GET_WAITING_LIST_BY_SCHEDULE, BOOK_WAITING_LIST, JWT_HEADER, GET_RESIDENCE_NUMBER} from "constants/urls";
 import axios from "axios";
-import {Button, Container, Card, Col, Row, Image, Form } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import {Button, Container, Card, Col, Row, Image, Form, Spinner } from "react-bootstrap";
 import PlusImage from "../../images/pasien/plus.png";
 import {useParams} from "react-router";
+import InvalidScheduleDate from "./InvalidScheduleDate";
 import { set } from "js-cookie";
 
 
@@ -23,12 +23,14 @@ const BookWaitingList = () => {
   const [currentWaitingList, setCurrentWaitingList] = React.useState("");
   const [totalWaitingList, setTotalWaitingList] = React.useState("");
   const [residenceNumber, setResidenceNumber] = React.useState("");
-  const [message, setMessage] = React.useState("");
+  const [invalidMessage, setInvalidMessage] = React.useState("");
+  const [successMessage, setSuccessMessage] = React.useState("");
   const [errorDate, setErrorDate] = React.useState("");
   const [errorSchedule, setErrorSchedule] = React.useState("");
   const [errorResidenceNumber, setErrorResidenceNumber] = React.useState("");
   const [isSelf, setIsSelf] = React.useState("");
-  
+  const [isLoading, setIsLoading] = React.useState(0);
+
   const getResidenceNumber = async () => {
     await axios
     .get(GET_RESIDENCE_NUMBER(), {
@@ -38,8 +40,7 @@ const BookWaitingList = () => {
       setResidenceNumber(res.data.residence_number);
     })
     .catch((err) => {
-      setMessage(err.response.data?.message ? err.response.data.message : "");
-      console.log(err);
+      setErrorResidenceNumber(err.response.data?.message ? err.response.data.message : "");
     });
   };
 
@@ -54,6 +55,7 @@ const BookWaitingList = () => {
   };
 
   const fetchData = async () => {
+    setIsLoading(true);
     await axios
       .get(GET_WAITING_LIST_BY_SCHEDULE(schedule_id, date), {
         headers: { Authorization: `Bearer ${JWT_HEADER}` },
@@ -66,13 +68,13 @@ const BookWaitingList = () => {
         setCurrentWaitingList(res.data.waiting_list.current_number);
         setTotalWaitingList(res.data.waiting_list.latest_number);
         setErrorResidenceNumber("");
-        setMessage("");
-        console.log(res.data);
+        setSuccessMessage("");
+        setInvalidMessage("");
       })
       .catch((err) => {
-        setMessage(err.response.data.message);
-        console.log(err);
+        setInvalidMessage(err.response.data.message);
       });
+      setIsLoading(false);
   };
 
   const _onBook = () => {
@@ -83,12 +85,9 @@ const BookWaitingList = () => {
         residence_number: residenceNumber,
       }, {headers: { Authorization: `Bearer ${JWT_HEADER}` }})
       .then((res) => {
-        setMessage(res.data.message);
-        console.log(res.data);
+        setSuccessMessage(res.data.message);
       })
       .catch((err) => {
-        console.log(err);
-        console.log(err.response.data);
         if (err.response) {
           if (err.response.data?.residence_number || err.response.data?.date || err.response.data?.schedule) {
             setErrorResidenceNumber(
@@ -105,85 +104,98 @@ const BookWaitingList = () => {
       });
   };
 
+  const onInvalid = (message) => {
+    return InvalidScheduleDate(message);
+  }
+
   return (
     <div className="mx-4 mt-3">
       <Container className="pasien-body py-2">
-            <Card
-              className="mx-lg-4 border border-0"
-              style={{
-                borderRadius: "20px",
-              }}
-            >
-              <Card.Body className="justify-content-between text-capitalize bg-white rounded">
-                <Row>
-                  <Col lg="6">
-                    <div className="card-body">
-                      <div>
-                        <h3>{healthAgency}</h3>
-                        <h4>{polyclinic}</h4>
-                        <p>{day}, {regDate}</p>
-                      </div>
-                      <br></br>
-                      <div>
-                        <p>Antrian Saat Ini</p>
-                        <p style={{marginTop: "0px"}}>Sedang Diperiksa/Antrian Terakhir</p>
-                        <h3>{currentWaitingList}/{totalWaitingList}</h3>
-                      </div>
-                    </div>
-                </Col>
-                <Col lg="6" className="d-flex align-items-center">
-                  <Form style={{
-                    padding: "20px"
-                  }}>
-                    <Form.Group controlId="exampleForm.ControlSelect1">
-                      <br></br>
-                      {message}
-                      {errorDate}
-                      {errorSchedule}
-                      <br></br>
-                      <Form.Label>Pilih Pendaftar</Form.Label>
-                      <Form.Control
-                        as="select"
-                        onChange={onChooseSelf.bind(this)}
+        <Card
+          className="mx-lg-4 border border-0"
+          style={{
+            borderRadius: "20px",
+            }}
+          >
+        {isLoading ? (
+          <Spinner animation="grow" variant="info" className="mx-auto">
+            <span className="sr-only">Loading...</span>
+          </Spinner>
+        ) : invalidMessage ? onInvalid(invalidMessage) : (
+          <Card.Body className="justify-content-between text-capitalize bg-white rounded">
+            <Row>
+              <Col lg="6">
+                <div className="card-body">
+                  <div>
+                    <h3>{healthAgency}</h3>
+                    <h4>{polyclinic}</h4>
+                    <p>{day}, {regDate}</p>
+                  </div>
+                  <br></br>
+                  <div>
+                    <p>Antrian Saat Ini</p>
+                    <p style={{marginTop: "0px"}}>Sedang Diperiksa/Antrian Terakhir</p>
+                    <h3>{currentWaitingList}/{totalWaitingList}</h3>
+                  </div>
+                </div>
+              </Col>
+              <Col lg="6" className="d-flex align-items-center">
+                {successMessage}
+                <br></br>
+                <Form style={{
+                  padding: "20px"
+                }}>
+                  <Form.Group controlId="exampleForm.ControlSelect1">
+                    <br></br>
+                    {errorDate}
+                    {errorSchedule}
+                    <br></br>
+                    <Form.Label>Pilih Pendaftar</Form.Label>
+                    <Form.Control
+                      as="select"
+                      onChange={onChooseSelf.bind(this)}
+                    >
+                      <option>Pilih Pendaftar</option>
+                      <option value="1">Diri Sendiri</option>
+                      <option value="2">Orang Lain</option>
+                    </Form.Control>
+                  </Form.Group>
+
+                  <Form.Group controlId="exampleForm.ControlInput1">
+                    <Form.Label>Masukkan NIK</Form.Label>
+                    <Form.Control 
+                      type="text" 
+                      disabled={isSelf? true : false} 
+                      placeholder="Masukkan NIK" 
+                      value={residenceNumber? residenceNumber : ""}
+                      onChange={(e) => {
+                        setResidenceNumber(e.target.value);
+                        setErrorResidenceNumber("");
+                      }} />
+                      {errorResidenceNumber}
+                  </Form.Group>
+
+                  <div className="row justify-content-center mb-5 mt-4">
+                    <div className="col-md-12">
+                      <Button
+                        className="rounded"
+                        variant="light"
+                        block
+                        onClick={_onBook}
                       >
-                        <option>Pilih Pendaftar</option>
-                        <option value="1">Diri Sendiri</option>
-                        <option value="2">Orang Lain</option>
-                      </Form.Control>
-                    </Form.Group>
-                    <Form.Group controlId="exampleForm.ControlInput1">
-                      <Form.Label>Masukkan NIK</Form.Label>
-                      <Form.Control 
-                        type="text" 
-                        disabled={isSelf? true : false} 
-                        placeholder="Masukkan NIK" 
-                        value={residenceNumber? residenceNumber : ""}
-                        onChange={(e) => {
-                          setResidenceNumber(e.target.value);
-                          setErrorResidenceNumber("");
-                        }} />
-                        {errorResidenceNumber}
-                    </Form.Group>
-                    <div className="row justify-content-center mb-5 mt-4">
-                      <div className="col-md-12">
-                        <Button
-                          className="rounded"
-                          variant="light"
-                          block
-                          onClick={_onBook}
-                        >
-                          <Image width="80px" src={PlusImage} />
-                          <span style={{ paddingLeft: "20px" }}>
-                            Daftar Antrian
-                          </span>
-                        </Button>
-                      </div>
+                        <Image width="80px" src={PlusImage} />
+                        <span style={{ paddingLeft: "20px" }}>
+                          Daftar Antrian
+                        </span>
+                      </Button>
                     </div>
-                  </Form>
-                  </Col>
-                </Row>
-              </Card.Body>
-            </Card>
+                  </div>
+                </Form>
+              </Col>
+            </Row>
+          </Card.Body>
+        )}
+        </Card>
       </Container>
     </div>
   );
