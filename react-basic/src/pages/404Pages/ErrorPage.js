4bcb8972c58534ac5038713abcd602731a574bd8
satriyoaji <satriyoaji27@gmail.com>
2020-11-14 01:30:50 +0700

    add import Link from react router Fixes #172

--- react-basic/src/pages/404Pages/ErrorPage.js
+++ react-basic/src/pages/404Pages/ErrorPage.js
@@ -1,48 +1,46 @@
-import React from 'react';
+import React from "react";
 import NavBar from "../../components/NavBar";
-import headerImage from "images/pasien/human sit.png";
 import FooterBar from "../../components/FooterBar";
-import Header from "../../components/admin/Header";
+import { Link } from "react-router-dom";
 
-ErrorPage.propTypes = {
-
-};
+ErrorPage.propTypes = {};
 
 function ErrorPage(props) {
-    return (
-        <div>
-{/*            {!props.isAdmin ?  (
+  return (
+    <div>
+      {/*            {!props.isAdmin ?  (
                     <Header/>
                 ) : (
                     <NavBar/>
             )}*/}
-            <NavBar/>
-            <div className="main-content">
-                <div className="row row-grid justify-content-center">
-                    <div className="">
-                        <div className="container min-vh-100 d-flex justify-content-center align-items-center">
-                            <div className="row">
-                                <div className="col-lg-12">
-                                    <div className="text-center">
-                                        <h1 className="display-4">404 ERROR</h1>
-                                        <p className="lead">Laman Tidak Ditemukan :(</p>
-                                        <h4 className="description text-default">Sepertinya Anda tersesat.</h4>
-                                    </div>
-                                    <div className="btn-wrapper text-center px--3">
-                                        <Link href="/"
-                                           className="btn btn-neutral mb-3 mb-sm-0">
-                                            <span className="btn-inner--text">Back To Home</span>
-                                        </Link>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+      <NavBar />
+      <div className="main-content">
+        <div className="row row-grid justify-content-center">
+          <div className="">
+            <div className="container min-vh-100 d-flex justify-content-center align-items-center">
+              <div className="row">
+                <div className="col-lg-12">
+                  <div className="text-center">
+                    <h1 className="display-4">404 ERROR</h1>
+                    <p className="lead">Laman Tidak Ditemukan :(</p>
+                    <h4 className="description text-default">
+                      Sepertinya Anda tersesat.
+                    </h4>
+                  </div>
+                  <div className="btn-wrapper text-center px--3">
+                    <Link to="/login" className="btn btn-neutral mb-3 mb-sm-0">
+                      <span className="btn-inner--text">Back To Home</span>
+                    </Link>
+                  </div>
                 </div>
+              </div>
             </div>
-            <FooterBar/>
+          </div>
         </div>
-    );
+      </div>
+      <FooterBar />
+    </div>
+  );
 }
 
 export default ErrorPage;
